3b2b83588fb7cc3f4d1d689e639996c1eba4c9fe
Lucas <lucas.carpentier24@gmail.com>
2026-04-13 15:47:06 +0200

    AJout du slotWords

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,26 @@ import BlobButton from "./components/BlobButton/BlobButton";
 import SlotWords from "./components/SlotsWords";
 import "./App.scss";
 import { ChevronDown, Mouse } from "lucide-react";
+import { useState, useEffect } from "react";
 
 export default function App() {
+  const [index, setIndex] = useState(0);
+  const [isExiting, setIsExiting] = useState(false);
+  const wordList = ["accessible", "moderne", "professionnel"];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsExiting(true); // Lance slideOut
+
+      setTimeout(() => {
+        setIndex((prevIndex) => (prevIndex + 1) % wordList.length);
+        setIsExiting(false); // Lance slideIn
+      }, 300); // Demi-durée de l'animation
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
       <section className="hero-section">
--- src/components/SlotsWords.jsx
+++ src/components/SlotsWords.jsx
@@ -1,8 +1,28 @@
+import { useState, useEffect } from "react";
+
 export default function SlotWords() {
+  const [index, setIndex] = useState(0);
+  const [isExiting, setIsExiting] = useState(false);
   const wordList = ["accessible", "moderne", "professionnel"];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsExiting(true);
+
+      setTimeout(() => {
+        setIndex((prev) => (prev + 1) % wordList.length);
+        setIsExiting(false);
+      }, 300);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="slot-words">
-      <span className="actual-span">{wordList[0]}</span>
+      <span key={index} className={`actual-span ${isExiting ? "exit" : ""}`}>
+        {wordList[index]}
+      </span>
       <span className="comma-span">,</span>
     </div>
   );
