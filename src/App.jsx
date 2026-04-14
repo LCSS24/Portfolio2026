e8a410cccbe82b85f4dd9e1e08e0e7291c60f964
Lucas C <lucas.carpentier24@gmail.com>
2026-04-15 00:50:00 +0200

    Refactor App component and create Hero component with animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,14 @@
-import BlobShader from "./components/BlobShader/BlobShader";
-import BlobButton from "./components/BlobButton/BlobButton";
-import SlotWords from "./components/SlotWords/SlotWords";
 import "./App.scss";
-import { ChevronDown, Mouse } from "lucide-react";
-import { useState, useEffect } from "react";
+import Hero from "./components/Hero/Hero";
 
 export default function App() {
-  const [index, setIndex] = useState(0);
-  const [isExiting, setIsExiting] = useState(false);
-  const wordList = ["accessible", "moderne", "professionnel"];
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setIsExiting(true); // Lance slideOut
-
-      setTimeout(() => {
-        setIndex((prevIndex) => (prevIndex + 1) % wordList.length);
-        setIsExiting(false); // Lance slideIn
-      }, 300); // Demi-durée de l'animation
-    }, 2000);
-
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <>
-      <section className="hero-section">
-        <BlobShader />
-        <div className="hero-container">
-          <div className="sentence">
-            <h2>Si votre site doit être</h2>
-            <SlotWords />
-            <h2>vous êtes au bon endroit</h2>
-          </div>
-          <BlobButton
-            label={"Démarrer un projet"}
-            onClick={console.log("caca")}
-          />
-        </div>
+      <Hero />
+      <section className="project-section">
+        <h3>PROJETS</h3>
+        <h4>OneMoreGame</h4>
       </section>
-      <div className="hero-bottom">
-        <Mouse />
-        <ChevronDown />
-      </div>
     </>
   );
 }
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,47 @@
+import BlobShader from "../BlobShader/BlobShader";
+import BlobButton from "../BlobButton/BlobButton";
+import SlotWords from "../SlotWords/SlotWords";
+import { ChevronDown, Mouse } from "lucide-react";
+import { useState, useEffect } from "react";
+import "./Hero.scss";
+
+export default function Hero() {
+  const [index, setIndex] = useState(0);
+  const [isExiting, setIsExiting] = useState(false);
+  const wordList = ["accessible", "moderne", "professionnel"];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsExiting(true); // Lance slideOut
+
+      setTimeout(() => {
+        setIndex((prevIndex) => (prevIndex + 1) % wordList.length);
+        setIsExiting(false); // Lance slideIn
+      }, 300); // Demi-durée de l'animation
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+  return (
+    <section>
+      <div className="hero-section">
+        <BlobShader />
+        <div className="hero-container">
+          <div className="sentence">
+            <h2>Si votre site doit être</h2>
+            <SlotWords />
+            <h2>vous êtes au bon endroit</h2>
+          </div>
+          <BlobButton
+            label={"Démarrer un projet"}
+            onClick={console.log("caca")}
+          />
+        </div>
+      </div>
+      <div className="hero-bottom">
+        <Mouse />
+        <ChevronDown />
+      </div>
+    </section>
+  );
+}
